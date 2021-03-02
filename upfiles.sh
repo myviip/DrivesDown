58957c6a0b665aa9a19bb6fe02a7b523dd6e2254
Gladtbam <49943258+Gladtbam@users.noreply.github.com>
2021-03-03 00:32:06 +0800

    Nourin 农林 	TV

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://nyaa.si/download/863473.torrent
+wget https://nyaa.si/download/860003.torrent
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 #for ((i=1, j=15; i<=685; i=i+15, j=j+15))
 #do
