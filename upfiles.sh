e7711895bcb76cc0f374d9868a437fde692473a7
Gladtbam <49943258+Gladtbam@users.noreply.github.com>
2021-03-02 23:11:17 +0800

    Bakuman. 爆漫王。 	TV(S1-3)

--- upfiles.sh
+++ upfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget https://nyaa.si/download/881576.torrent
+wget https://nyaa.si/download/876893.torrent
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
-for ((i=1, j=15; i<=218; i=i+15, j=j+15))
+for ((i=1, j=20; i<=576; i=i+20, j=j+20))
 do
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
 rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ nice:/视频/
