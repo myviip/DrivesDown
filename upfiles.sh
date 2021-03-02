d75a6630d516883cf5557c51d0c222ec1cd2aff1
Gladtbam <49943258+Gladtbam@users.noreply.github.com>
2021-03-02 23:28:09 +0800

    Hai to Gensou no Grimgar 灰与幻想的格林姆迦尔 	TV

--- upfiles.sh
+++ upfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget https://nyaa.si/download/875469.torrent
+wget https://nyaa.si/download/867872.torrent
 #aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
-for ((i=1, j=15; i<=161; i=i+15, j=j+15))
+for ((i=1, j=15; i<=271; i=i+15, j=j+15))
 do
 aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
 rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ nice:/视频/
