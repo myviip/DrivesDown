53aa458f7a28d455ef12740d32f0537cff32fa95
Gladtbam <49943258+Gladtbam@users.noreply.github.com>
2021-03-02 22:26:39 +0800

    Kyoukai no Kanata 境界的彼方 	TV MOVIE(M1-2)

--- upfiles.sh
+++ upfiles.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-wget https://nyaa.si/download/887574.torrent
-aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
-#for ((i=1, j=20; i<=341; i=i+20, j=j+20))
-#do
-#aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
+wget https://nyaa.si/download/885374.torrent
+#aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
+for ((i=1, j=20; i<=341; i=i+20, j=j+20))
+do
+aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
 rclone move -v --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ nice:/视频/
-#echo "$i-$j 上传完成"
-#done
+echo "$i-$j 上传完成"
+done
